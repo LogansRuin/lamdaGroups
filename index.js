a6dc0789fd7b7758f0b2335512174fe2b2c1af39
Jared <jared@needapart.co.nz>
2019-07-07 20:36:52 +1200

    allocate even number of balanced number of students into groups

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-const groupStudents = () => {
-  const students = ['andre', 'anisha', 'ash', 'celia', 'davidb', 'davidn', 'keith', 'jennie', 'joanna', 'jared', 'raaya', 'rahul', 'ruslan', 'taine', 'tamari', 'noel']
-
-  //shuffle array
+const groupStudents = (groupNames, students) => {
+  //shuffle array - Fisher-Yates algorithm
   for (let i = students.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
     [students[i], students[j]] = [students[j], students[i]]; // swap elements
   }
+  console.log('Shuffled students: ', students, '\n')
 
-  console.log(students)
+  //Check how many groups there are
+  const numOfGroups = groupNames.length
+  console.log('Number of groups: ',numOfGroups, '\n')
+  
+  // how many in each group?
+  const numInBase = Math.floor(students.length / numOfGroups)
+  const extras = students.length % numOfGroups || 'none'
+  console.log(`Base number in each group: ${numInBase} with ${extras} left over`,'\n')
+
+  // Allocate everyone to a group
+  const allocate = () => {
+    const group = []
+    for (let i = 0; i < numInBase; i++) {
+    group.push(students.shift())
+    }
+    return group
+  }
+
+  groupNames.map(group => {
+    if(extras === 'none'){
+      console.log( {
+        name: group,
+        students: allocate()
+      })
+    }
+  })
+
+console.log(groupNames)
 }
 
-groupStudents()
+const students = ['andre', 'anisha', 'ash', 'celia', 'davidb', 'davidn', 'keith', 'jennie', 'joanna', 'jared', 'raaya', 'rahul', 'ruslan', 'taine', 'tamari', 'noel']
+
+const groupNames = ['1','2','3','4']
+
+groupStudents(groupNames, students)
